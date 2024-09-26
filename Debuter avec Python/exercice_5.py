5238d38c1e5ac306ed47ca5e14fdc9d8534a0186
nicolashudon <nicolashu@lyceechurchill.london>
2024-09-26 19:28:06 +0000

    exercice 5 2.0

--- Debuter avec Python/exercice_5.py
+++ Debuter avec Python/exercice_5.py
@@ -1,7 +1,22 @@
-#Exercice 4 / question 5
-def perimetre_cercle(x) :
-    from math import pi
-    return 2 * pi * x
-x=3
-print("Exercice 4 / question 5:")
-print("Donc, le perimetre du cercle de rayon : ", x , " est de :", perimetre_cercle(x))
+import random
+import math
+
+#Exercice 5 / question 1
+print("Exercice 5 / question 1:")
+print("1. La formule pour calculer le perimetre d'une cercle est: 2πr ")
+
+
+#Exercice 5 / question 2
+print("Exercice 5 / question 2:")
+print("2. L'argument est le rayon du cercle. Cet argument serait de type float")
+
+
+#Exercice 5 / question 3
+r = random.randint(1, 10)
+pi = (math.pi)
+def perimetre_cercle(r):
+    perimetre = 2 * pi * r
+    perimetreApprox = round(perimetre, 2)
+    return perimetreApprox
+print("Exercice 5 / question 3:")
+print("Le perimetre du cercle de rayon", r , "est de environ ", perimetre_cercle(r))
