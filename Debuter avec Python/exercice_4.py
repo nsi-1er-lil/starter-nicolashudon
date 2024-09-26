91fca1bd934b80c131413e553a15af81e79d8006
nicolashudon <nicolashu@lyceechurchill.london>
2024-09-26 19:00:39 +0000

    exercice 5

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -6,6 +6,7 @@ c = random.randint(1, 10)
 P = c * 4
 A = c ** 2
 b = A > 5
+print("Exercice 4 / question 6:")
 print("On prend ", c, "comme la longueure d'un cote")
 print("Le perimetre du carre est de", P)
 print("L'aire du carre est de", A )
@@ -17,12 +18,13 @@ x = random.randint(1, 10)
 def perimetre(x):
     return x * 4
 
-
+print("Exercice 4 / question 7:")
 print("Le perimetre du carre, calculer avec une fonction, en utilisant", x, "comme longueure d'un cote, est de", perimetre(x))
 
 #Exercice 4 / question 8
 def surface(x):
     return x ** 2
 
+print("Exercice 4 / question 8:")
 print("L'aire dur carre, calculer avec une fonction, en utilisant", x, "comme longueure d'un cote, est de", surface(x))
 
---
+++ Debuter avec Python/exercice_5.py
@@ -0,0 +1,7 @@
+#Exercice 4 / question 5
+def perimetre_cercle(x) :
+    from math import pi
+    return 2 * pi * x
+x=3
+print("Exercice 4 / question 5:")
+print("Donc, le perimetre du cercle de rayon : ", x , " est de :", perimetre_cercle(x))
