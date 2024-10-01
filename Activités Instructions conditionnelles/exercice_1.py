84406a432d4e101ce2dc0b17fa8d4a0c69a1a372
nicolashudon <nicolashu@lyceechurchill.london>
2024-10-01 10:12:20 +0000

    exercice_1.py (AIC)

---
+++ Activités Instructions conditionnelles/exercice_1.py
@@ -0,0 +1,6 @@
+import random
+taille = random.randit(0,270)
+if taille > 150 :
+    print(taille, 'cm : Vous pouver entrer, ne vomissez pas svp.')
+else:
+    print(taille, 'cm : Vous etes trop petits, allez manger de la soupe.')
